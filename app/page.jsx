d59af2834166b712910c15c2846ccb35f7d6481d
egue harris <harrisegue9@gmail.com>
2025-03-20 23:20:59 -0700

    about added

--- app/page.jsx
+++ app/page.jsx
@@ -11,11 +11,11 @@ export default function Home() {
 
         <Link href={"/"}>
           <button className=" bg-white text-lightRed font-bold w-[160px] py-3 rounded-md uppercase text-sm">
-            Buy $ABSMAN
+            Buy Now
           </button>
         </Link>
       </nav>
-      <section className=" relative text-white px-5 py-16 pt-32">
+      <section className=" relative text-white px-5 py-16 pt-32 ">
         <div className=" absolute w-full h-full top-0 left-0">
           <div className=" absolute w-full bg-black h-full opacity-90" />
           <img
@@ -43,6 +43,44 @@ export default function Home() {
           </div>
           <div className=" mt-10">
             <img src="/absman.png" alt="absman" className=" mx-auto" />
+          </div>
+        </div>
+      </section>
+      <section className=" bg-lightGreen px-5 py-16 text-white">
+        <h2 className=" uppercase text-center text-white text-5xl abt relative w-fit mx-auto">About</h2>
+        <div className=" mt-10">
+          <div>
+            <img src="about.jpeg" alt="about image" className=" rounded-md" />
+          </div>
+          <div className=" mt-5 about-text text-lg">
+            <p>
+              Once a brilliant developer, he was betrayed by the very system he
+              sought to safeguard. A catastrophic breach fused him with the
+              Abstract code, turning him into something more—a silent sentinel
+              for a digital age.
+              The Ruggers, merciless thieves of the crypto world, thrive in the
+              dark corners of the decentralized web. They exploit, they destroy,
+              leaving only ruin in their wake. But Absman fights back, not as a
+              hero, but as a necessary evil.
+            </p>
+          </div>
+        </div>
+        <div className=" mt-10">
+          <div>
+            <img src="about-2.jpg" alt="about image" className=" rounded-md" />
+          </div>
+          <div className=" mt-5 about-text text-lg">
+            <p>
+              He strikes from the void,
+              dismantling their schemes with cold precision, leaving nothing but
+              fear in his wake.
+              He doesn’t wear the Abstract logo as a symbol of hope—it’s a
+              warning. To the criminals lurking in the code, to those who prey
+              on the innocent, it means one thing: Your time is running out.
+              Absman knows the truth: In this world, there are no second
+              chances. Only the strong survive, and the strong do what’s
+              necessary. He will protect the network—no matter the cost.
+            </p>
           </div>
         </div>
       </section>
